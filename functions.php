d9b2c277a6121b4474419074655ec3f9afcc8f2f
Rafi <ryagudin@gmail.com>
2015-03-27 01:36:56 -0600

    experimenting with header address & phone

--- functions.php
+++ functions.php
@@ -23,7 +23,7 @@ function theme_enqueue_styles() {
 function append_html_post_nav( $nav_output ){
 
 
-    $html = '<div style="margin-left: 10px; float: left; font-size: 15px;">Recreational & Medicinal | 920 W 104th Ave, Northglenn CO | <a href="tel:303-379-9526">303.379.9526</a></div>';
+    $html = '<div style="margin-left: 10px; float: left; font-size: 15px;"><a href="https://www.google.com/maps/place/39%C2%B053'00.7%22N+104%C2%B059'50.4%22W/@39.8841324,-104.997564,18z/data=!4m2!3m1!1s0x0:0x0?hl=en-US">920 W 104th Ave, Northglenn CO</a> | <a href="tel:303-379-9526">303.379.9526</a></div>';
 
     return $nav_output . $html;
 
